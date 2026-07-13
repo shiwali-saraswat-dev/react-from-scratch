2167a8ea5e50a5b083b158857472b8778fa7f0c6
Shiwali Saraswat <saraswatshiwali@gmail.com>
2026-07-13 19:38:52 +0530

    feat: nested list in React

---
+++ 004-nested-list-react/App.js
@@ -0,0 +1,29 @@
+/* Do the below HTML in React:
+<div id="parent">
+    <div id="child">
+        <h1>I am H1 tag</h1>
+    </div>
+    <div id="child2">
+        <h1>I am H1 tag</h1>
+        <h2>I am H2 tag</h2>
+    </div>
+</div> */
+
+
+const div = React.createElement("div", {id: "parent"}, 
+    [
+        React.createElement("div", {id: "child"}, 
+            React.createElement("h1", {}, "I am H1 tag")
+        ),
+        React.createElement("div", {id: "child2"}, 
+            [
+                React.createElement("h1", {}, "I am H1 tag"),
+                React.createElement("h2", {}, "I am H2 tag")
+            ]
+        )
+    ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(div);
